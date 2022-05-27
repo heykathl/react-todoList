0ad79600f867472f5604023439634120c75534b1
Kathleen Ly <heykathl@users.noreply.github.com>
2022-05-27 18:47:34 +0200

    Stores and retrieves todos from local drive with useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList.js';
 import { v4 as uuid } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
+  // getting stored todos from local drive
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, []) //empty array will call this once, will not be recalled
+
+  // stores in local drive
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos]) //runs everytime anything in array changes
+
   function handleAdd(e){
     const name = todoNameRef.current.value
     if(name === "") return
+    // loads all todos plus new one gets added
     setTodos(prevTodos => {
       return [...prevTodos, {id: uuid(), name: name, complete: false}]
     })
