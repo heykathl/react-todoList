1e6d08acda41feb2705bb2d741ae5337501c6560
Kathleen Ly <heykathl@users.noreply.github.com>
2022-05-27 18:42:48 +0200

    Created handleAdd function, incorporated uuid and clears when clicked on add

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import React, { useState } from 'react';
-import TodoList from './TodoList.js'
+import React, { useState, useRef } from 'react';
+import TodoList from './TodoList.js';
+import { v4 as uuid } from 'uuid';
 
 function App() {
-  const [todos, setTodos] = useState([{id: 1, name: 'Todo 1', complete: false }])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAdd(e){
+    const name = todoNameRef.current.value
+    if(name === "") return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuid(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null // clear inputs
+  }
+
   return (
     <>
       <TodoList todos={todos}/>
-      <input type="text"/>
-      <button>Add</button>
+      <input ref={todoNameRef} type="text"/>
+      <button onClick={handleAdd}>Add</button>
       <button>Clear</button>
       <div>0 left to do</div>
     </>
